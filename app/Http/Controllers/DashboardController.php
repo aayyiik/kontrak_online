30f20809f3b0669620bae3ab55284780cc71a03b
Ari Lathifatul Chusna <ari.lathifatul.chusna-2020@vokasi.unair.ac.id>
2023-03-25 10:47:02 +0700

    templates

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     function index(){
-        return view('dashboard.index');
+        return view('templates.master');
     }
 }
