116cba47554e1a294d5b2756a9ef9b8a47bc2b9e
Ari Lathifatul Chusna <ari.lathifatul.chusna-2020@vokasi.unair.ac.id>
2023-04-14 11:10:10 +0700

    unit seeder

--- database/seeders/UnitKerjaSeeder.php
+++ database/seeders/UnitKerjaSeeder.php
@@ -12,6 +12,23 @@ class UnitKerjaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $unit = [
+            [
+                'id_unit' => 'U001',
+                'nama_unit' => 'Pengadaan Jasa'
+            ],
+            [
+                'id_unit'=> 'U002',
+                'nama_unit' => 'PPBJ'
+            ],
+            [
+                'id_unit' => 'U003',
+                'nama_unit' => 'Keuangan dan Sekretariat'
+            ],
+            [
+                'id_unit' => 'U004',
+                'nama_unit' => 'Hukum'
+            ]
+            ];
     }
 }
